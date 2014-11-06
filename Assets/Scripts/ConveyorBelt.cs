b11f978849d85ba82c4f72ab6e4be37b5860f034
mebrianlin <me.brian.lin+ad@gmail.com>
2014-11-06 17:05:34 -0500

    Added test project repository
    ^

---
+++ Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorBelt : MonoBehaviour {
+
+    FoodFactory _factory;
+
+	// Use this for initialization
+	void Start () {
+        _factory = new FoodFactory();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/FoodFactory.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class FoodFactory {
+
+    public enum FoodType
+    {
+        Meat,
+        Vegetable,
+        Sauce
+    }
+
+    public sealed class Food
+    {
+        public Food(string name, FoodType type)
+        {
+            this.Name = name;
+            this.Type = type;
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public FoodType Type
+        {
+            get;
+            private set;
+        }
+
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public Food CreateFood()
+    {
+        return new Food("mushroom", FoodType.Vegetable);
+    }
+
+    public Food CreateFood(FoodType type)
+    {
+        return new Food("mushroom", FoodType.Vegetable);
+    }
+
+}
