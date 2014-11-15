d0df2967a9e28a63908752ac0f2e95cdb4362f1e
Eric Tsai <ekt7be@virginia.edu>
2014-11-15 16:52:14 -0500

    Adjusted PS Movement+AttachNoodles
    PS moves up and down faster, and can move horizontally too now.
    AttachNoodles need to be adjusted as a result

---
+++ Assets/Scripts/AttachLeftNoodleScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachLeftNoodleScript : MonoBehaviour
+{
+		public bool attached = false;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnTriggerEnter (Collider col)
+		{
+				if (col.tag == "LeftTableTrigger")
+						attached = true;
+		}
+
+		void OnTriggerExit (Collider col)
+		{
+				if (col.tag == "LeftTableTrigger")
+						attached = false;
+		}
+
+		public void unAttach ()
+		{
+				attached = false;
+		}
+
+		public bool isAttached ()
+		{
+				return attached;
+		}
+
+}
