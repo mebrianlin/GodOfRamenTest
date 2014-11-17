18316964582063c2ed265e930f416dcc36e8b77e
Eric Tsai <ekt7be@virginia.edu>
2014-11-17 12:08:28 -0500

    Start button sprite logic implemented

--- Assets/Scripts/StartScreenScript.cs
+++ Assets/Scripts/StartScreenScript.cs
@@ -7,10 +7,19 @@ public class StartScreenScript : MonoBehaviour
 		// Use this for initialization
 		private bool leftButtonClicked = false;
 		private bool rightButtonClicked = false;
-
+		private GameObject leftButton;
+		private GameObject leftButtonDown;
+		private GameObject rightButton;
+		private GameObject rightButtonDown;
 		void Start ()
 		{
 	
+				leftButton = this.transform.Find ("left").gameObject;
+				leftButtonDown = this.transform.Find ("left_2").gameObject;
+				rightButton = this.transform.Find ("right").gameObject;
+				rightButtonDown = this.transform.Find ("right_2").gameObject;
+				rightButtonDown.SetActive (false);
+				leftButtonDown.SetActive (false);
 		}
 	
 		// Update is called once per frame
@@ -19,10 +28,15 @@ public class StartScreenScript : MonoBehaviour
 				if (leftButtonClicked && rightButtonClicked)
 						Application.LoadLevel (Application.loadedLevel + 1);
 
-				if (Input.GetMouseButtonDown (0))
+				if (Input.GetMouseButtonDown (0)) {
 						leftButtonClicked = true;
-
-				if (Input.GetMouseButtonDown (1))
+						leftButton.SetActive (false);
+						leftButtonDown.SetActive (true);
+				}
+				if (Input.GetMouseButtonDown (1)) {
 						rightButtonClicked = true;
+						rightButton.SetActive (false);
+						rightButtonDown.SetActive (true);
+				}
 		}
 }
