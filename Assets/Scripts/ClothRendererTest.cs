9d9664d224b915b4f15a7a511c2e975e6e89aa75
mebrianlin <me.brian.lin+ad@gmail.com>
2014-11-16 19:37:01 -0500

    scene ramen changed by eric

--- Assets/Scripts/ClothRendererTest.cs
+++ Assets/Scripts/ClothRendererTest.cs
@@ -22,7 +22,10 @@ public class ClothRendererTest : MonoBehaviour
 		public GameObject[] rightHand;
 		public GameObject openLeftHand;
 		public GameObject openRightHand;
+        public GameObject perfectText;
+        public GameObject ouchText;
 
+        
 		public float slope;
 
 		private float topHeight;
@@ -69,6 +72,7 @@ public class ClothRendererTest : MonoBehaviour
 
 				openLeftHand.SetActive (false);
 				openRightHand.SetActive (false);
+                turnOffText();
 		
 				_renderer = noodles.GetComponent<Renderer> ();
 				topHeight = topBar.GetComponent<Transform> ().position.y;
@@ -134,6 +138,8 @@ public class ClothRendererTest : MonoBehaviour
 								if (maxRamenHeight > bottomHeight && maxRamenHeight < topHeight) {
 										isRamenWithinRange = true;
 										++noodleHitCount;
+                                        showPerfectText();
+                                        Invoke("turnOffText", 1f);
 								}
 						}
 
@@ -146,7 +152,10 @@ public class ClothRendererTest : MonoBehaviour
 								Invoke ("resetNoodles", 2f);
 								attachedToHands = false;
 								if (!mute)
-										SoundManager.instance.playRamenBreak ();		
+										SoundManager.instance.playRamenBreak ();
+
+                                showOuchText();
+                                Invoke("turnOffText",1f);
 						}		
 		
 						if (noodleHitCount >= maxNoodleHitCount) {// noodleScore >= maxNoodleScore) {
@@ -220,6 +229,28 @@ public class ClothRendererTest : MonoBehaviour
 				openLeftHand.SetActive (false);
 				openRightHand.SetActive (false);
 		}
+
+
+        
+        void showPerfectText()
+        {
+            perfectText.SetActive(true);
+            ouchText.SetActive(false);
+        }
+
+        void showOuchText()
+        {
+            ouchText.SetActive(true);
+            perfectText.SetActive(false);
+        }
+
+        void turnOffText()
+        {
+            ouchText.SetActive(false);
+            perfectText.SetActive(false);
+            //yield return new WaitForSeconds(1f);
+        }
+
 		//add to ramen score as a linear function y=kx where y is addOnScore, and x is distance. 
 		//addOnScore is then added onto the noodleScore
 		void addNoodleScore (float distance)
