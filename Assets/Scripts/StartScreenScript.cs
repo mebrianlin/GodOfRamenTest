fcd234b0830876883bbab21fbbb4a943eaf5e6f7
Eric Tsai <ekt7be@virginia.edu>
2014-11-17 11:52:05 -0500

    Added Start scene

---
+++ Assets/Scripts/StartScreenScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenScript : MonoBehaviour
+{
+
+		// Use this for initialization
+		private bool leftButtonClicked = false;
+		private bool rightButtonClicked = false;
+
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (leftButtonClicked && rightButtonClicked)
+						Application.LoadLevel (Application.loadedLevel + 1);
+
+				if (Input.GetMouseButtonDown (0))
+						leftButtonClicked = true;
+
+				if (Input.GetMouseButtonDown (1))
+						rightButtonClicked = true;
+		}
+}
