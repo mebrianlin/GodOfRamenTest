7b53432352e5e0ccd1dd1d4cd8ef51aefe8c5ad1
mebrianlin <me.brian.lin+ad@gmail.com>
2014-11-19 19:57:59 -0500

    change game to 30 seconds

--- Assets/Scripts/Common/GameSettings.cs
+++ Assets/Scripts/Common/GameSettings.cs
@@ -27,7 +27,7 @@ public static class GameSettings
     };
     static Dictionary<string, int> _intTable = new Dictionary<string, int>()
     { 
-        { "TimePerRound", 60 },
+        { "TimePerRound", 30 },
         { "MaxNoodleHitCount", 4 },
     };
     static Dictionary<string, string> _stringTable = new Dictionary<string, string>();
