33f007d3baa05ca788aa4fb959f8f75448ee8e69
mebrianlin <me.brian.lin+ad@gmail.com>
2014-11-19 19:55:50 -0500

    calibration screen

---
+++ Assets/Scripts/Common/ChangeToScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeToScene : MonoBehaviour {
+    public string NextSceneName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel(NextSceneName);
+		}
+	}
+}
+
